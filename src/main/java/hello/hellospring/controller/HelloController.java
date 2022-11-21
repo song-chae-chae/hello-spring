a76c1335701863236290e8a659d7cc404c9cf363
chaechae <songchaechaes@gmail.com>
2022-11-21 18:10:41 +0900

    add HelloController and template pages

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,50 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        // Model은 스프링에서 넣어줌
+        // 넘어온거에 addAttribute로 값을 넣음
+        model.addAttribute("data", "hello!");
+        // 리턴하면 스프링부트가 resources/template/hello.html로 찾도록 해놨음
+        return "hello";
+    }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http 응답 바디로 return 값을 넘기겠다
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
